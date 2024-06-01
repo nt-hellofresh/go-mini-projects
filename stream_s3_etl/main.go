65cc50ccdfe680f5b30cf727bd210272b34810f1
Neville Tummon <neville.tummon@hellofresh.com.au>
2024-06-01 22:35:29 +1000

    add non-working example

--- stream_s3_etl/main.go
+++ stream_s3_etl/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"io"
 	"log"
 	"os"
@@ -10,6 +13,7 @@ import (
 func main() {
 	//main1()
 	main2()
+	//main3()
 }
 
 func main1() {
@@ -52,4 +56,35 @@ func main2() {
 	}
 
 	log.Printf("Successfully processed %d bytes from input to output", n)
+}
+
+// This example doesn't work
+func main3() {
+	args := getArgs()
+	args.validate()
+
+	r := getS3ObjectBody(args)
+	defer r.Close()
+
+	pipe, err := NewJsonBatchPipe(r, handleTransformLogic)
+
+	if err != nil {
+		log.Fatal(fmt.Errorf("failed to create new json batch pipe: %w", err))
+	}
+
+	client := newS3Client(args.awsProfileName)
+
+	// Can't stream an io.Reader of unknown size to S3, which is a shame.
+	// Reference issue: https://github.com/aws/aws-sdk-go/issues/122#issuecomment-76826724
+	result, err := client.PutObject(context.Background(), &s3.PutObjectInput{
+		Bucket: args.bucket,
+		Key:    aws.String("output/transformed.json"),
+		Body:   pipe,
+	})
+
+	if err != nil {
+		log.Println(fmt.Errorf("failed to put object: %w", err))
+	}
+
+	log.Println(result)
 }
